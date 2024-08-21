713511273a326bb503698aee106a62dbc12144d8
Casper Holten <casho@itu.dk>
2024-08-21 18:39:13 +0200

    More readable

--- BrokenLinkChecker/DocumentParsing/Browsing/HtmlParserPool.cs
+++ BrokenLinkChecker/DocumentParsing/Browsing/HtmlParserPool.cs
@@ -1,5 +1,4 @@
 using System.Collections.Concurrent;
-using AngleSharp;
 using AngleSharp.Html.Parser;
 
 namespace BrokenLinkChecker.DocumentParsing.Browsing
@@ -11,20 +10,18 @@ namespace BrokenLinkChecker.DocumentParsing.Browsing
 
         public HtmlParserPool(HtmlParserOptions config, int maxPoolSize = 1)
         {
-            _parserPool = new ConcurrentBag<IHtmlParser>();
-            _semaphore = new SemaphoreSlim(maxPoolSize, maxPoolSize);
-
-            // Pre-fill the pool with the exact number of parsers
+            _parserPool = [];
+            _semaphore = new SemaphoreSlim(maxPoolSize);
+            
             for (int i = 0; i < maxPoolSize; i++)
             {
-                var parser = new HtmlParser(config);
+                HtmlParser parser = new HtmlParser(config);
                 _parserPool.Add(parser);
             }
         }
 
         public async Task<PooledHtmlParser> GetParserAsync()
         {
-            // Wait until a parser is available in the pool
             await _semaphore.WaitAsync();
 
             _parserPool.TryTake(out IHtmlParser parser);
@@ -34,7 +31,7 @@ namespace BrokenLinkChecker.DocumentParsing.Browsing
         internal void ReturnParser(IHtmlParser parser)
         {
             _parserPool.Add(parser);
-            _semaphore.Release(); // Release a slot in the semaphore to signal availability
+            _semaphore.Release();
         }
     }
 }
