c7b71a6a686b9c171f8b0f1314de99e2e329caf4
casperh123 <casho@itu.dk>
2025-04-11 23:02:47 +0200

    Add email provider

---
+++ WebsiteAnalyzer.Web/Services/MailSenderProvider.cs
@@ -0,0 +1,45 @@
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using MimeKit;
+
+namespace WebsiteAnalyzer.Web.Services;
+
+public class MailSenderProvider : IEmailSender
+{
+    private readonly IConfiguration _configuration;
+
+    public MailSenderProvider(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+    
+    public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+    {
+        string? smtpUsername = _configuration["MAILSENDER_USERNAME"];
+        string? smtpPassword = _configuration["MAILSENDER_PASSWORD"];
+        
+        if (smtpUsername is null || smtpPassword is null)
+        {
+            throw new AuthenticationException();
+        }
+        
+        MimeMessage message = new MimeMessage();
+        
+        message.From.Add(new MailboxAddress("Clypper's Website Analyzer", "noreply@clyppertechnology.com"));
+        message.To.Add(new MailboxAddress("", email));
+        message.Subject = subject;
+        message.Body = new TextPart("html")
+        {
+            Text = htmlMessage
+        };
+
+        using SmtpClient client = new SmtpClient();
+        
+        await client.ConnectAsync("smtp.mailersend.net", 587);
+        await client.AuthenticateAsync(smtpUsername, smtpPassword);
+            
+        await client.SendAsync(message);
+        await client.DisconnectAsync(true); 
+    }
+}
