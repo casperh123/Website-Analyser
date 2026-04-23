fd6bf9fe5065125bd9c4f8a1702fc22a7c1e00b9
casperh123 <casperholten@protonmail.com>
2026-04-23 11:59:47 +0200

    Add kereby back

--- src/WebsiteAnalyzer.Services/Services/AppartmentService.cs
+++ src/WebsiteAnalyzer.Services/Services/AppartmentService.cs
@@ -1,3 +1,4 @@
+using System.Net.Http.Json;
 using System.Text.Json;
 using WebsiteAnalyzer.Application.Services;
 using WebsiteAnalyzer.Services.Cache;
@@ -28,14 +29,23 @@ public class AppartmentService(HttpClient client, MailService mailService)
             .GetProperty("items")
             .EnumerateArray();
 
-        var tenancies = items.Select(item => new TenancyDto
+        var cejTenancies = items.Select(item => new TenancyDto
         {
             Id = item.GetProperty("id").GetString()!,
             Url = $"https://udlejning.cej.dk/boliger/{item.GetProperty("id").GetString()}",
             Name = item.GetProperty("name").GetString()!
         });
+        
+        JoratoResponse? kerebyResponse = await client.GetFromJsonAsync<JoratoResponse>("https://api.jorato.com/tenancies?visibility=public&showAll=true&key=2gXoBtKvFMMgKJ1VBJ5G5pNr2GD");
 
-        var unseen = tenancies
+        IEnumerable<TenancyDto> kerebyTenancies = kerebyResponse?.items
+            .Where(IsAvailable)
+            .Where(IsResidential)
+            .Select(tenancy => new TenancyDto(tenancy))
+            .ToList() ?? [];
+
+        List<TenancyDto> unseen = cejTenancies
+            .Concat(kerebyTenancies)
             .Where(t => !_cache.Exists(t.Id))
             .ToList();
 
@@ -50,8 +60,8 @@ public class AppartmentService(HttpClient client, MailService mailService)
 
             try
             {
-                await mailService.SendEmailAsync("clypper.tech@protonmail.com", "CEJ Lejlighed Tilgængelig", body);
-                await mailService.SendEmailAsync("ie@live.dk", "CEJ Lejlighed Tilgængelig", body);
+                await mailService.SendEmailAsync("casperholten@protonmail.com", "Nye lejligheder Tilgængelige", body);
+                await mailService.SendEmailAsync("ie@live.dk", "Nye lejligheder Tilgængelige", body);
             }
             catch
             {
@@ -61,6 +71,9 @@ public class AppartmentService(HttpClient client, MailService mailService)
 
         return unseen;
     }
+    
+    public bool IsAvailable(KerebyTenancy a) => a.state == "Available";
+    public bool IsResidential(KerebyTenancy t) => t.classification == "Residential";
 }
 
 public class TenancyDto
@@ -68,4 +81,53 @@ public class TenancyDto
     public string Id { get; set; }
     public string Url { get; set; }
     public string Name { get; set; }
+
+    public TenancyDto()
+    {
+    }
+
+    public TenancyDto(KerebyTenancy tenancy)
+    {
+        Id = tenancy.id;
+        Url = tenancy.Url;
+        Name = tenancy.title;
+    }
+}
+
+public class JoratoResponse
+{
+    public List<KerebyTenancy> items { get; set; } = [];
+}
+
+public class KerebyTenancy
+{
+    public string state { get; set; } = string.Empty;
+    public string classification { get; set; } = string.Empty;
+    public string id { get; set; } = string.Empty;
+    public string title { get; set; } = string.Empty;
+    
+    public Address address { get; set; } = new Address();
+    public string Url => "https://kerebyudlejning.dk/bolig/" + address.flattened;
+
+    public class Address
+    {
+        public string street { get; set; } = string.Empty;
+        public string zipcode { get; set; } = string.Empty;
+        public string city { get; set; } = string.Empty;
+
+        public string flattened =>
+            (
+                street.Replace(" ", "-") +
+                "-" +
+                zipcode.Replace(" ", "-") +
+                "-" +
+                city.Replace(" ", "-")
+            )
+            .ToLower()
+            .Replace(".", "")
+            .Replace(",", "")
+            .Replace("ø", "o")
+            .Replace("å", "a")
+            .Replace("æ", "a");
+    }
 }
